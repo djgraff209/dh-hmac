2f2c9cd8a6c63ae24ba1c014d26b82442c6cc135
David Graff <djgraff209@gmail.com>
2016-02-18 02:09:27 -0500

    Fleshed out implementation
    Initialized MAC independently of DH exchange
    Used DH key agreement to establish AES wrap/unwrap of MAC SecretKey
    Refactored Alice and Bob to have more phased approach and compartmentalization of logic
    Updated pipes to have distinct communications channels - otherwise race condition led to one thread reading back information it just wrote.
    Utilized Java8 java.time constructs to provide timestamping

--- dh-hmac.groovy
+++ dh-hmac.groovy
@@ -1,77 +1,110 @@
-import java.io.*;
-import java.math.BigInteger;
-import java.security.*;
-import java.security.spec.*;
-import java.security.interfaces.*;
-import javax.crypto.*;
-import javax.crypto.spec.*;
-import javax.crypto.interfaces.*;
-import com.sun.crypto.provider.SunJCE;
-import javax.xml.bind.DatatypeConverter
-import java.nio.channels.*;
+import java.io.*
+import java.nio.channels.*
+import java.security.*
+import java.security.spec.*
+import java.security.interfaces.*
+import java.time.*
 import java.util.concurrent.CountDownLatch
+import javax.crypto.*
+import javax.crypto.spec.*
+import javax.crypto.interfaces.*
 
-def Pipe pipe= Pipe.open();
-final DataOutputStream sinkStream= new DataOutputStream(Channels.newOutputStream(pipe.sink().configureBlocking(true)));
-final DataInputStream sourceStream= new DataInputStream(Channels.newInputStream(pipe.source().configureBlocking(true)));
-def latch= new CountDownLatch(2)
 
 class Alice implements Runnable {
+    Integer dhKeyLength
     DataOutputStream sinkStream
     DataInputStream sourceStream
-    String message;
-    CountDownLatch latch;
+    CountDownLatch latch
+    String message
+    
+    KeyAgreement aliceKeyAgree
+    Mac aliceMac
+
+    private void establishKeyAgreement() {
+        AlgorithmParameterGenerator paramGen = AlgorithmParameterGenerator.getInstance("DH")
+        paramGen.init(dhKeyLength)
+        AlgorithmParameters params = paramGen.generateParameters()
+        DHParameterSpec dhParamSpec = (DHParameterSpec)params.getParameterSpec (DHParameterSpec.class)
+        
+        println "ALICE: Generate DH keypair ..."
+        KeyPairGenerator aliceKpairGen = KeyPairGenerator.getInstance("DH")
+        aliceKpairGen.initialize(dhParamSpec)
+        KeyPair aliceKpair = aliceKpairGen.generateKeyPair()
+        
+        // Alice creates and initializes her DH KeyAgreement object
+        println "ALICE: Initialization ..."
+        KeyAgreement keyAgree = KeyAgreement.getInstance("DH")
+        keyAgree.init(aliceKpair.getPrivate())
+        
+        // Alice encodes her public key, and sends it over to Bob.
+        print "ALICE: Send public ... "
+        sinkStream.writeUTF(aliceKpair.getPublic().getEncoded().encodeBase64().toString())
+
+        // Read bob's public key
+        byte[] bobPubKeyEnc= sourceStream.readUTF().decodeBase64()
+        println "ALICE: ... Receive public"
+
+        /*
+         * Alice uses Bob's public key for the first (and only) phase
+         * of her version of the DH
+         * protocol.
+         * Before she can do so, she has to instantiate a DH public key
+         * from Bob's encoded key material.
+         */
+        KeyFactory bobKeyFac = KeyFactory.getInstance("DH")
+        def x509KeySpec = new X509EncodedKeySpec(bobPubKeyEnc)
+        PublicKey bobPubKey = bobKeyFac.generatePublic(x509KeySpec)
+        println "ALICE: Execute PHASE1 ..."
+        keyAgree.doPhase(bobPubKey, true)
+        
+        aliceKeyAgree= keyAgree
+    }
+    
+    private void establishHmac() {
+        // Create an instance of HmacSHA512 key generator
+        KeyGenerator hmacKeyGenerator= KeyGenerator.getInstance("HmacSHA512")
+        // generate HmacSHA512 key
+        SecretKey aliceSigningKey= hmacKeyGenerator.generateKey()
+        
+        print "ALICE: Sending Mac Algo ${aliceSigningKey.algorithm} ... "
+        sinkStream.writeUTF(aliceSigningKey.algorithm)
+
+        // Create AES cipher to wrap the key
+        Cipher cipher= Cipher.getInstance("AES")
+        cipher.init(Cipher.WRAP_MODE, aliceKeyAgree.generateSecret("AES"))
+        // Wrap & Transmit the key
+        print "ALICE: Sending Mac SecretKey(${aliceSigningKey.algorithm}) ... "
+        sinkStream.writeUTF(cipher.wrap(aliceSigningKey).encodeBase64().toString())
+
+        // Create mac engine and initialize with key
+        aliceMac= Mac.getInstance("HmacSHA512")
+        aliceMac.init(aliceSigningKey)
+    }
 
     @Override
     public void run() {
         try {
-            AlgorithmParameterGenerator paramGen = AlgorithmParameterGenerator.getInstance("DH");
-            paramGen.init(512);
-            AlgorithmParameters params = paramGen.generateParameters();
-            DHParameterSpec dhSkipParamSpec = (DHParameterSpec)params.getParameterSpec (DHParameterSpec.class);
+            establishKeyAgreement()
+            establishHmac()
+
+            def odtString= OffsetDateTime.now(ZoneOffset.UTC).toString()
+            aliceMac.update(odtString.getBytes())
+
+            // Create & write mac 
+            def computedMac= aliceMac.doFinal(message.getBytes()).encodeBase64().toString()
+            sinkStream.writeUTF(computedMac)
+            print "ALICE: Sent Mac ${computedMac} ... "
             
-            System.out.println("ALICE: Generate DH keypair ...");
-            KeyPairGenerator aliceKpairGen = KeyPairGenerator.getInstance("DH");
-            aliceKpairGen.initialize(dhSkipParamSpec);
-            KeyPair aliceKpair = aliceKpairGen.generateKeyPair();
+            // Write message
+            sinkStream.writeUTF(odtString)
+
+            print "ALICE: Sent timestamp ${odtString} ... "
+            sinkStream.writeUTF(message)
+
+            print "ALICE: Sent message ${message} ... "
             
-            // Alice creates and initializes her DH KeyAgreement object
-            System.out.println("ALICE: Initialization ...");
-            KeyAgreement aliceKeyAgree = KeyAgreement.getInstance("DH");
-            aliceKeyAgree.init(aliceKpair.getPrivate());
-            
-            // Alice encodes her public key, and sends it over to Bob.
-            byte[] alicePubKeyEnc = aliceKpair.getPublic().getEncoded();
-            sinkStream.writeInt(alicePubKeyEnc.length);
-            sinkStream.write(alicePubKeyEnc, 0, alicePubKeyEnc.length);
-    
-            int inboundLen= sourceStream.readInt();
-            byte[] bobPubKeyEnc= new byte[inboundLen]
-            sourceStream.readFully(bobPubKeyEnc, 0, inboundLen);
-    
-            /*
-             * Alice uses Bob's public key for the first (and only) phase
-             * of her version of the DH
-             * protocol.
-             * Before she can do so, she has to instantiate a DH public key
-             * from Bob's encoded key material.
-             */
-            KeyFactory aliceKeyFac = KeyFactory.getInstance("DH");
-            def x509KeySpec = new X509EncodedKeySpec(bobPubKeyEnc);
-            PublicKey bobPubKey = aliceKeyFac.generatePublic(x509KeySpec);
-            System.out.println("ALICE: Execute PHASE1 ...");
-            aliceKeyAgree.doPhase(bobPubKey, true);
-    
-            SecretKeySpec aliceSigningKey= new SecretKeySpec(aliceKeyAgree.generateSecret(), "HmacSHA512")
-            Mac aliceMac= Mac.getInstance("HmacSHA512")
-            aliceMac.init(aliceSigningKey)
-    
-            byte[] aliceResult= aliceMac.doFinal(message.getBytes());
-            sinkStream.writeInt(aliceResult.length)
-            sinkStream.write(aliceResult, 0, aliceResult.length);
-            sinkStream.writeUTF(message);
-            
-            println "Bob said: "+sourceStream.readUTF()
+            // See what bob said
+            println "ALICE: Bob said: ${sourceStream.readUTF()}"
         }
         finally {
             latch.countDown()
@@ -82,66 +115,110 @@ class Alice implements Runnable {
 class Bob implements Runnable {
     DataOutputStream sinkStream
     DataInputStream sourceStream
-    CountDownLatch latch;
+    CountDownLatch latch
+
+    KeyAgreement bobKeyAgree
+    Mac bobMac
+
+    private establishKeyAgreement() {
+        byte[] alicePubKeyEnc= sourceStream.readUTF().decodeBase64()
+        println "BOB: Receive public"
+
+        /*
+         * Let's turn over to Bob. Bob has received Alice's public key
+         * in encoded format.
+         * He instantiates a DH public key from the encoded key material.
+         */
+        KeyFactory bobKeyFac = KeyFactory.getInstance("DH")
+        X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(alicePubKeyEnc)
+        PublicKey alicePubKey = bobKeyFac.generatePublic(x509KeySpec)
+
+        /*
+         * Bob gets the DH parameters associated with Alice's public key.
+         * He must use the same parameters when he generates his own key
+         * pair.
+         */
+        DHParameterSpec dhParamSpec = ((DHPublicKey)alicePubKey).getParams()
+
+        // Bob creates his own DH key pair
+        println "BOB: Generate DH keypair ..."
+        KeyPairGenerator bobKpairGen = KeyPairGenerator.getInstance("DH")
+        bobKpairGen.initialize(dhParamSpec)
+        KeyPair bobKpair = bobKpairGen.generateKeyPair()
+
+        // Bob creates and initializes his DH KeyAgreement object
+        println "BOB: Initialization ..."
+        KeyAgreement keyAgree = KeyAgreement.getInstance("DH")
+        keyAgree.init(bobKpair.getPrivate())
+
+        // Bob encodes his public key, and sends it over to Alice.
+        print "BOB: Write public key ... "
+        sinkStream.writeUTF(bobKpair.getPublic().getEncoded().encodeBase64().toString())
+
+        /*
+         * Bob uses Alice's public key for the first (and only) phase
+         * of his version of the DH
+         * protocol.
+         */
+        println "BOB: Execute PHASE1 ..."
+        keyAgree.doPhase(alicePubKey, true)
+        bobKeyAgree= keyAgree
+    }
+    
+    private establishHmac() {
+        // Create cipher to unwrap secret key with
+        Cipher cipher= Cipher.getInstance("AES")
+        cipher.init(Cipher.UNWRAP_MODE, bobKeyAgree.generateSecret("AES"))
+        
+        String macAlgorithm = sourceStream.readUTF()
+        println "BOB: Received Mac Algo ${macAlgorithm}"
+        // Read and unwrap secret key
+        SecretKey signingKey= cipher.unwrap(sourceStream.readUTF().decodeBase64(), macAlgorithm, Cipher.SECRET_KEY)
+        println "BOB: Recieved Mac SecretKey(${signingKey.algorithm})"
+
+        // Create mac engine
+        bobMac= Mac.getInstance(macAlgorithm)
+        // Initialize with the signing key
+        bobMac.init(signingKey)
+    }
 
     @Override
     public void run() {
         try {
-            int inboundLen= sourceStream.readInt();
-            byte[] alicePubKeyEnc= new byte[inboundLen]
-            sourceStream.readFully(alicePubKeyEnc, 0, inboundLen);
-            /*
-             * Let's turn over to Bob. Bob has received Alice's public key
-             * in encoded format.
-             * He instantiates a DH public key from the encoded key material.
-             */
-            KeyFactory bobKeyFac = KeyFactory.getInstance("DH");
-            X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(alicePubKeyEnc);
-            PublicKey alicePubKey = bobKeyFac.generatePublic(x509KeySpec);
+            establishKeyAgreement()
+            establishHmac()
     
-            /*
-             * Bob gets the DH parameters associated with Alice's public key.
-             * He must use the same parameters when he generates his own key
-             * pair.
-             */
-            DHParameterSpec dhParamSpec = ((DHPublicKey)alicePubKey).getParams();
-    
-            // Bob creates his own DH key pair
-            System.out.println("BOB: Generate DH keypair ...");
-            KeyPairGenerator bobKpairGen = KeyPairGenerator.getInstance("DH");
-            bobKpairGen.initialize(dhParamSpec);
-            KeyPair bobKpair = bobKpairGen.generateKeyPair();
-    
-            // Bob creates and initializes his DH KeyAgreement object
-            System.out.println("BOB: Initialization ...");
-            KeyAgreement bobKeyAgree = KeyAgreement.getInstance("DH");
-            bobKeyAgree.init(bobKpair.getPrivate());
-    
-            // Bob encodes his public key, and sends it over to Alice.
-            byte[] bobPubKeyEnc = bobKpair.getPublic().getEncoded();
-            sinkStream.writeInt(bobPubKeyEnc.length);
-            sinkStream.write(bobPubKeyEnc, 0, bobPubKeyEnc.length);
-    
-            /*
-             * Bob uses Alice's public key for the first (and only) phase
-             * of his version of the DH
-             * protocol.
-             */
-            System.out.println("BOB: Execute PHASE1 ...");
-            bobKeyAgree.doPhase(alicePubKey, true);
-    
-            SecretKeySpec bobSigningKey= new SecretKeySpec(bobKeyAgree.generateSecret(), "HmacSHA512")
-            Mac bobMac= Mac.getInstance("HmacSHA512")
-            bobMac.init(bobSigningKey)
-    
+            // Read the mac
+            def computedMac= sourceStream.readUTF()
+            println "BOB: Receved Mac ${computedMac}"
+            byte[] aliceResult= computedMac.decodeBase64()
             
-            byte[] aliceResult= new byte[sourceStream.readInt()];
-            sourceStream.readFully(aliceResult)
-            def message = sourceStream.readUTF();
+            // Read timestamp
+            def odtString= sourceStream.readUTF()
+            bobMac.update(odtString.getBytes())
+            println "BOB: Received timestamp ${odtString}"
             
-            byte[] bobResult= bobMac.doFinal(message.getBytes())
-            assert aliceResult == bobResult
-            sinkStream.writeUTF("OK")
+            // Read the message
+            def message = sourceStream.readUTF()
+            println "BOB: Received message ${message}"
+
+            OffsetDateTime now= OffsetDateTime.now(ZoneOffset.UTC)
+            OffsetDateTime then= OffsetDateTime.parse(odtString)
+            
+            // TODO: temporal distance check - discard old messages?
+            if( Duration.between(then, now).getSeconds() > 10 ) {
+                sinkStream.writeUTF("!OK")
+            }
+            else {
+                // Compute mac on message
+                byte[] bobResult= bobMac.doFinal(message.getBytes())
+                
+                // Compare result
+                assert aliceResult == bobResult
+                
+                // Respond OK
+                sinkStream.writeUTF("OK")
+            }
         }
         finally {
             latch.countDown()
@@ -149,12 +226,22 @@ class Bob implements Runnable {
     }
 }
 
-Thread t1= new Thread(new Alice(message: "The quick brown fox jumped over the lazy dog", sinkStream: sinkStream, sourceStream: sourceStream, latch: latch))
-Thread t2= new Thread(new Bob(sinkStream: sinkStream, sourceStream: sourceStream, latch: latch))
+def Pipe aliceToBob= Pipe.open()
+def Pipe bobToAlice= Pipe.open()
+def latch= new CountDownLatch(2)
+
+final Integer dhKeyLength=1024
+
+Thread t1= new Thread(new Alice(dhKeyLength: 1024, 
+                                message: "The quick brown fox jumped over the lazy dog", 
+                                sinkStream: new DataOutputStream(Channels.newOutputStream(aliceToBob.sink().configureBlocking(true))), 
+                                sourceStream: new DataInputStream(Channels.newInputStream(bobToAlice.source().configureBlocking(true))), 
+                                latch: latch))
+Thread t2= new Thread(new Bob(sinkStream: new DataOutputStream(Channels.newOutputStream(bobToAlice.sink().configureBlocking(true))), 
+                                sourceStream: new DataInputStream(Channels.newInputStream(aliceToBob.source().configureBlocking(true))), 
+                                latch: latch))
 t1.start()
 t2.start()
 
 latch.await()
 println "done"
-
-
